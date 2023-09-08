32b74d5ea8ba04dea6cb0910efd5934f77c53dc8
FrankPabloGalan29 <142623502+FrankPabloGalan29@users.noreply.github.com>
2023-09-08 13:58:02 -0500

    Add files via upload

---
+++ src/t1_pregunta_08/T1_Pregunta_08.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1_pregunta_08;
+
+/**
+ *
+ * @author Frank
+ */
+public class T1_Pregunta_08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona per = new Persona();
+        per.setLocationRelativeTo(null);
+        per.setVisible(true);
+    }
+    
+}
